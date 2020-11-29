8c722cdc92061332b1e943a0381a9ca373d83ce6
yasuaki640 <42242014+yasuaki640@users.noreply.github.com>
2020-11-29 21:05:35 +0900

    learn var declaration let, var, const

--- src/var_decralation.js
+++ src/var_decralation.js
@@ -11,3 +11,36 @@ console.log(val1);
 // 再宣言可能
 var val1 = "再宣言可能";
 console.log(val1);
+
+let val2 = "let";
+console.log(val2);
+
+// letは上書き可能
+val2 = "let2";
+console.log(val2);
+
+// let val2 = "再宣言できない";
+
+const val3 = "const変数";
+
+// val3 = "上書きできない";
+// const val3 = "再宣言できない";
+
+// constで定義したオブジェクトはプロパティは変更可能
+const val4 = {
+  name: "yasu",
+  age: 26
+};
+console.log(val4);
+
+val4.name = "uwagaki";
+console.log(val4);
+
+// 配列も変更できる、、、
+const val5 = ["dog", "cat"];
+
+val5[0] = "bird";
+
+val5.push("monkey");
+
+console.log(val5);
